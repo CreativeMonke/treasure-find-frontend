e51770c4a0dc4899c0e9a4b0056bc494a4ce44d3
CreativeMonke <63314157+CreativeMonke@users.noreply.github.com>
2024-03-17 14:01:57 +0200

    Live Location Heading
    Implement icon rotation based on heading

--- src/components/Location/LiveLocation.jsx
+++ src/components/Location/LiveLocation.jsx
@@ -4,53 +4,75 @@ import L from "leaflet";
 import "leaflet-control-geocoder/dist/Control.Geocoder.js";
 import { UNSAFE_DataRouterStateContext } from "react-router-dom";
 
-const liveLocationIcon = new L.Icon({
-    iconUrl: "./Icons/GpsArrow.png", // URL to your custom icon image
-    iconSize: [25, 41], // Size of the icon
-    iconAnchor: [12, 41], // Anchor point of the icon to ensure it points to the exact location
-    popupAnchor: [1, -34], // Position of the popup relative to the icon
+L.Icon.Rotated = L.Icon.extend({
+  _setIconStyles: function (img, name, anchor) {
+    L.Icon.prototype._setIconStyles.apply(this, arguments);
+    if (name === "icon" && this.options.iconAngle) {
+      img.style[L.DomUtil.TRANSFORM] +=
+        " rotate(" + this.options.iconAngle + "deg)";
+    }
+  },
 });
 
-function LiveLocation()
-{
-    const [liveMarker,setLiveMarker] = useState(null);
-    const map = useMap();
-    
-    useEffect(() => {
-        function updateLiveLocation() {
-            navigator.geolocation.watchPosition((location) =>{
-                const {latitude,longitude} = location.coords;
-                const latlng = L.latLng(latitude,longitude);
+const liveLocationIcon = new L.Icon({
+  iconUrl: "./icons/GpsArrow.png", // URL to your custom icon image
+  iconSize: [25, 41], // Size of the icon
+  iconAnchor: [12, 41], // Anchor point of the icon to ensure it points to the exact location
+  popupAnchor: [1, -34], // Position of the popup relative to the icon
+  iconAngle: 0,
+});
 
-                if(liveMarker)
-                {
-                    liveMarker.setLatLng(latlng);
-                }
-                else
-                {
-                    const newLiveMarker = L.marker(latlng, {icon: liveLocationIcon})
-                    .addTo(map)
-                    .bindPopup("You are here");
+function LiveLocation() {
+  const [liveMarker, setLiveMarker] = useState(null);
+  const map = useMap();
 
-                    setLiveMarker(newLiveMarker);
-                }
-            },(err)=>{
-                console.error(err);
-            },{
-                enableHighAccuracy: true,
-                maximumAge: 10000,
-                timeout: 5000,
-            });
+  useEffect(() => {
+    function updateLiveLocation() {
+      navigator.geolocation.watchPosition(
+        (location) => {
+          const { latitude, longitude, heading } = location.coords;
+          const latlng = L.latLng(latitude, longitude);
+
+          if (liveMarker) {
+            liveMarker.setLatLng(latlng);
+            map.flyTo(latlng, 16);
+
+            liveMarker.setIcon(
+              new L.Icon.Rotated({
+                iconUrl: "/icons/GpsArrow.png",
+                iconSize: [25, 41],
+                iconAnchor: [12, 41],
+                popupAnchor: [1, -34],
+                iconAngle: heading,
+              })
+            );
+          } else {
+            const newLiveMarker = L.marker(latlng, { icon: liveLocationIcon })
+              .addTo(map)
+              .bindPopup("You are here");
+
+            setLiveMarker(newLiveMarker);
+            map.flyTo(latlng, 16);
+          }
+        },
+        (err) => {
+          console.error(err);
+        },
+        {
+          enableHighAccuracy: true,
+          maximumAge: 10000,
+          timeout: 5000,
         }
+      );
+    }
 
-        updateLiveLocation();
+    updateLiveLocation();
 
-        return() =>{
-            if(liveMarker)
-            liveMarker.remove();
-        };
-    },[map,liveMarker]);
-    return null;
-}   
+    return () => {
+      if (liveMarker) liveMarker.remove();
+    };
+  }, [map, liveMarker]);
+  return null;
+}
 
 export default LiveLocation;
