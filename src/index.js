9c87b67444b64347a464e2b3a07e7039f95df3e6
CreativeMonke <63314157+CreativeMonke@users.noreply.github.com>
2024-04-16 03:08:52 +0300

    Added vercel speed insights

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./i18n.js"
 import { Provider } from "react-redux";
 import { store } from "./app/store.js"
 import { Analytics } from '@vercel/analytics/react';
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
@@ -14,5 +14,6 @@ root.render(
     <Provider store={store}>
         <App />
         <Analytics />
+        <SpeedInsights />
     </Provider>
 );
